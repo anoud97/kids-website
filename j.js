655800522f451d1336746ec3b6e6756687f2b9f8
Anoud Yahya Alghamdi <aygh97@gmail.com>
2020-12-01 20:24:15 +0300

    Add files via upload

---
+++ j.js
@@ -0,0 +1,73 @@
+function question1() {
+	
+var value1 = document.getElementById("pass1").value;
+
+  if (value1 == 1) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again");
+  }
+}
+function question2() { 
+
+  var value2 = document.getElementById("pass2").value;
+  if (value2 == 3) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question3() { 
+
+  var value3 = document.getElementById("pass3").value;
+  if (value3 == 2) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question4() { 
+
+  var value4 = document.getElementById("pass4").value;
+  if (value4 == 2) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question5() { 
+
+  var value5 = document.getElementById("pass5").value;
+  if (value5 == 0) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question6() { 
+
+  var value6 = document.getElementById("pass6").value;
+  if (value6 == 1) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question7() { 
+
+  var value7 = document.getElementById("pass7").value;
+  if (value7 == 4) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
+function question8() { 
+
+  var value8 = document.getElementById("pass8").value;
+  if (value8 == 6) {
+     window.alert("very good ;)");
+  } else {
+     window.alert("try again :( ");
+  }
+}
